2b86db595ecfca24cc031cc40bbf2b4e090e7751
ALFONSO CABELLO <alfonso.cabelloluna@gmail.com>
2021-07-05 22:40:45 -0400

    Tab in devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
 
-devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+	devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
 end
