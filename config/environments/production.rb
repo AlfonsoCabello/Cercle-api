eadff1327b5d1b3852418f83cf2ef38442ca618f
ALFONSO CABELLO <alfonso.cabelloluna@gmail.com>
2021-07-30 02:11:52 -0400

    Add email info to production enviroments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,24 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 , protocol: 'http'}
+
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => "cercleapi.users@gmail.com",
+    :password => "rbvbdhwcllwpghay",
+    :authentication => "plain",
+    :enable_starttls_auto => true
+  }
+
+
+
 end
